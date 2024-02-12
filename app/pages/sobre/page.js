492748694f1ea69a584f78766c8c99b36368ef71
unknown <giulianobotelho@outlook.com.br>
2024-02-12 13:56:47 -0300

    text adjust

--- app/pages/sobre/page.js
+++ app/pages/sobre/page.js
@@ -13,19 +13,19 @@ export default function Agenda() {
         <h2 className='ano'>2 0 2 4</h2>
         </article>
 
-        <section>
 
         <h2 style={{textAlign:'center'}}>Ministério Apostólico Bethesda</h2>
+     <section className='texto'>
         <p>
-        Nós somos uma igreja na visão celular no modelo dos 12 desde 2010. Entendemos que a estrutura para uma 
-        igreja essencial parte de uma mentalidade de reino na qual as pessoas precisam de um discipulado e que a medida que as pessoas se convertem, elas entendem que precisam reproduzir o reino de Deus através do evangelismo pessoal, de uma vida mudada, santificada em Deus. A estrutura da visão atende a
-         nossa expectativa porque ela tem o principio de ganhar, consolidar, discipular e enviar.
+        Nós somos uma igreja na visão celular, no modelo dos 12, desde 2010. 
+        Entendemos que a estrutura essencial para uma igreja parte de uma mentalidade de reino na qual as pessoas precisam de discipulado.
+         À medida que as pessoas se convertem, entendem que precisam reproduzir o reino de Deus através do evangelismo pessoal e de uma vida mudada e santificada em Deus. 
+         A estrutura da visão atende à nossa expectativa porque tem o princípio de ganhar, consolidar, discipular e enviar.
          </p>
          <p>
-A historia da igreja é baseada nisso, uma visão para um ministério apostólico, que entende que a partir do ganhar, 
-consolidar e discipular desenvolvendo um trabalho sobre a vida do novo cristão ao longo da sua vida em Deus, ele descobrirá
- os seus dons espirituais. O apostólico compreende sobre os 5 dons ministeriais: apóstolos, profetas, evangelistas, pastores e mestres.
-  (Efésios 4:11-13). A partir desses 5 ministérios nós entendemos que uma igreja precisa trabalhar esse chamado sobre a vida do discípulo.</p>
+         A história da igreja é baseada nisso: uma visão para um ministério apostólico que entende que, a partir do ganhar, consolidar e discipular, desenvolvendo um trabalho sobre a vida do novo cristão ao longo da sua vida em Deus,
+          ele descobrirá seus dons espirituais. O apostólico compreende os cinco dons ministeriais: apóstolos, profetas, evangelistas, pastores e mestres (Efésios 4:11-13). 
+          A partir desses cinco ministérios, entendemos que uma igreja precisa trabalhar esse chamado sobre a vida do discípulo.</p>
         
   </section>
     </main>
